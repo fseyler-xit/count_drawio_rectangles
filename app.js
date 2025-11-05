7e10c4d16f85188204d74d661c313f8df98f49b0
Frank S <frank.seyler@x-it-service.de>
2025-11-05 11:14:40 +0100

    Init

---
+++ app.js
@@ -0,0 +1,19 @@
+// numbers
+x = 42;
+pi = 3.1415
+
+// String
+text = 'Hallo Welt';
+text = "Hallo Welt";
+answer = `Ergebnis: ${pi}`;
+
+// Boolean
+
+isSunny = true;
+isRainy = false;
+
+// undefined
+x = undefined;
+
+// null
+x = null;
